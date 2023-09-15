06ed7dc1c709c624c4036d3c52ed6f816ca410af
Md. Mintu Mia <mintumia8@gmail.com>
2023-09-16 00:42:31 +0600

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <h1>About page !</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <h1>This text is from Index Page!</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
